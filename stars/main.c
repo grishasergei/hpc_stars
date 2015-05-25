4acd29a6bae9eaebd833c2a23eeaaf42f351dff9
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2015-05-25 22:12:03 +0200

    Initial Commit

---
+++ stars/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stars
+//
+//  Created by Sergii Gryshkevych on 25/05/15.
+//  Copyright (c) 2015 Sergii Gryshkevych. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
